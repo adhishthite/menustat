dd7da2e1291515a69ee8829d9616982a66fc622b
Adhish Thite <adhish.thite@gmail.com>
2026-05-28 18:12:33 +0530

    Use Next analytics entrypoint

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque, JetBrains_Mono } from "next/font/google";
 import Script from "next/script";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const display = Bricolage_Grotesque({
